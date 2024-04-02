f5aa77534d6c7395b69f5ba65cdf6cd684d664f2
Jakub969 <jakub.hrubizna@gmail.com>
2024-04-02 12:07:54 +0200

    posledné úpravy

--- bakalarska_praca/main.cpp
+++ bakalarska_praca/main.cpp
@@ -1,13 +1,7 @@
 #include "bakalarska_praca.h"
-#include <stdlib.h>  
-#include <crtdbg.h>
-#define _CRTDBG_MAP_ALLOC  
 
 int main(int argc, char *argv[])
 {
-#ifdef _DEBUG
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
     // kód ku grafickému rozhraniu
     QApplication a(argc, argv);
     bakalarska_praca w;
